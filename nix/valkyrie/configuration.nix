8f11a9d22a7f3a22ddceb166cd1f9691187cb583
toxicdefender404 <vincent.hills@gmail.com>
2025-03-22 22:39:52 +0000

    change script + learn to squash
    93 current  2025-03-22 22:39:49  24.11.20250320.7105ae3     6.6.83                          *

--- nix/valkyrie/configuration.nix
+++ nix/valkyrie/configuration.nix
@@ -211,8 +211,8 @@
   services.flatpak.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [25565];
-  networking.firewall.allowedUDPPorts = [25565];
+  networking.firewall.allowedTCPPorts = [];
+  networking.firewall.allowedUDPPorts = [];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
--- rebuild.sh
+++ rebuild.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-if git diff --quiet '*.nix'; then
-    echo "No changes detected, exiting."
-    popd
-    exit 0
+tracked_changes=$(git diff --name-only)
+untracked_files=$(git ls-files --others --exclude-standard)
+
+if [ -z "$tracked_changes" ] && [ -z "$untracked_files" ]; then
+  echo "No changes detected, exiting."
+  exit 0
 fi
 
-# Autoformat your nix files
+
 alejandra . &>/dev/null \
   || ( alejandra . ; echo "formatting failed!" && exit 1)
 
-# Shows your changes
-git diff -U0 '*.nix'
+echo "Staged changes:"
+git diff --staged -U0
 
 echo "NixOS Rebuilding..."
 
-# Rebuild, output simplified errors, log trackebacks
+# rebuild and output simplified errors
 sudo nixos-rebuild switch --flake ./nix &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
-# Get current generation metadata
+git add ./nix/
+
+# Get current generation metadata for commit message
 current=$(nixos-rebuild list-generations | grep current)
 
 git commit -am "$current"
