e022d5502ff1690ee53d181ad87ec93a50106881
toxicdefender404 <vincent.hills@gmail.com>
2025-03-22 22:09:12 +0000

    add basic update script

---
+++ rebuild.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if git diff --quiet '*.nix'; then
+    echo "No changes detected, exiting."
+    popd
+    exit 0
+fi
+
+# Autoformat your nix files
+alejandra . &>/dev/null \
+  || ( alejandra . ; echo "formatting failed!" && exit 1)
+
+# Shows your changes
+git diff -U0 '*.nix'
+
+echo "NixOS Rebuilding..."
+
+# Rebuild, output simplified errors, log trackebacks
+sudo nixos-rebuild switch --flake ./nix &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
